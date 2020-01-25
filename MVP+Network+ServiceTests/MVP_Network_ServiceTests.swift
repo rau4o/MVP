9a5a2edf1ef3d58283a099b212441041f5ff7e21
rau4o <rkoshkinbayev@gmail.com>
2020-01-25 18:17:34 +0600

    Initial Commit

---
+++ MVP+Network+ServiceTests/MVP_Network_ServiceTests.swift
@@ -0,0 +1,34 @@
+//
+//  MVP_Network_ServiceTests.swift
+//  MVP+Network+ServiceTests
+//
+//  Created by rau4o on 1/25/20.
+//  Copyright © 2020 rau4o. All rights reserved.
+//
+
+import XCTest
+@testable import MVP_Network_Service
+
+class MVP_Network_ServiceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
